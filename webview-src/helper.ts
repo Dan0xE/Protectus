eedfaf35982ebaa2f349a1dc38b58e61476e6c70
Daniel <contact@dan0xe.me>
2026-02-01 00:00:21 +0100

    refactor: prettify failed function name

--- webview-src/helper.ts
+++ webview-src/helper.ts
@@ -9,26 +9,34 @@ type GenericFunc<F> = () => Promise<F>;
 
 /** Checks if the required feature for the passed function is enabled on the rust backend
  * @param {keyof Features} featureEnabled Feature that has to be enabled for this function to be executed
- * @param {GenericFunc<F>[]} functionsToExecute Functions that should be executed if the required feature is enabled
+ * @param {GenericFunc<F>} functionToExecute Functions that should be executed if the required feature is enabled
  * @throws Throws if the required feature is not enabled in the rust backend
  */
 export async function featureGate<F>(
     featureEnabled: keyof Features,
-    ...functionsToExecute: GenericFunc<F>[]
+    functionToExecute: GenericFunc<F>
 ): Promise<F | undefined> {
     const features: Features = await invoke(
         'plugin:protectus|feature_check_command'
     );
 
     if (features[featureEnabled]) {
-        let lastResult: F | undefined = undefined;
-        for (const fn of functionsToExecute) {
-            lastResult = await fn();
-        }
-        return lastResult;
+        return await functionToExecute();
     }
 
+    // NOTE functionsToExecute will look something like this:
+    // async () => invoke('plugin:protectus|get_hwid_command')
+    // We only care about what's gets passed inside of the invoke(...) function
+    // *after* the 'plugin:protectus|' part.
+    //
+    // Brace your eyes for some regex magic:
+    const extractCommandName = (fn: GenericFunc<F>) =>
+        fn.toString().match(/invoke\('plugin:protectus\|([^']+)/)?.[1] ??
+        'unknown_function';
+
+    const functionName = extractCommandName(functionToExecute);
+
     throw new Error(
-        `${functionsToExecute} called but required feature ${featureEnabled} is not enabled!`
+        `${functionName} called but required feature ${featureEnabled} is not enabled!`
     );
 }
